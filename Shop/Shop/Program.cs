721b1d76cdcb4f9b3ee57b536134db03ba6edb2a
SuperSpiritEagle <118557578+SuperSpiritEagle@users.noreply.github.com>
2023-05-05 09:51:34 +0500

    GetMoney

--- Shop/Shop/Program.cs
+++ Shop/Shop/Program.cs
@@ -13,117 +13,155 @@ namespace Shop
 
             List<Product> products = new List<Product>(); ;
 
-            products.Add(new Product("фото"));
-            products.Add(new Product("ноут"));
-            products.Add(new Product("тел"));
-
             shop.WorkProgram(seller, player, products);
         }
+    }
 
-        class Shop
+    class Shop
+    {
+        public void WorkProgram(Seller seller, Player player, List<Product> products)
         {
-            public void WorkProgram(Seller seller, Player player, List<Product> products)
+            const string ComandShowProduct = "1";
+            const string ComandSellProduct = "2";
+            const string ComandSeeYourStuff = "3";
+            const string ComandExit = "exit";
+
+            bool isWork = true;
+
+            string userInput;
+
+            seller.Products(products);
+
+            while (isWork)
             {
-                const string ComandShowProduct = "1";
-                const string ComandSellProduct = "2";
-                const string ComandSeeYourStuff = "3";
-                const string ComandExit = "exit";
+                Console.WriteLine($"команда [{ComandShowProduct}] показать товар\n" +
+                                  $"команда [{ComandSellProduct}] продать товар\n" +
+                                  $"команда [{ComandSeeYourStuff}] посмотреть свой покупки\n" +
+                                  $"команда [{ComandExit}] для выхода из программы");
 
-                bool isWork = true;
+                userInput = Console.ReadLine();
+                Console.Clear();
 
-                string userInput;
-
-                while (isWork)
+                switch (userInput)
                 {
-                    Console.WriteLine($"команда [{ComandShowProduct}] показать товар\n" +
-                                      $"команда [{ComandSellProduct}] продать товар\n" +
-                                      $"команда [{ComandSeeYourStuff}] посмотреть свой покупки\n" +
-                                      $"команда [{ComandExit}] для выхода из программы");
+                    case ComandShowProduct:
+                        seller.ShowProducts(products, ConsoleColor.Green);
+                        break;
 
-                    userInput = Console.ReadLine();
-                    Console.Clear();
+                    case ComandSellProduct:
 
-                    switch (userInput)
-                    {
-                        case ComandShowProduct:
-                            seller.ShowProducts(products,ConsoleColor.Blue);
-                            break;
+                        seller.SellProduct(products, seller, player);
+                        break;
 
-                        case ComandSellProduct:
-                            seller.SellProduct(products, seller, player);
-                            break;
+                    case ComandSeeYourStuff:
+                        player.ShowPurchase();
+                        break;
 
-                        case ComandSeeYourStuff:
-                            player.ShowPurchase();
-                            break;
-
-                        case ComandExit:
-                            isWork = false;
-                            break;
-                    }
+                    case ComandExit:
+                        isWork = false;
+                        break;
                 }
             }
         }
-        class Persone
-        {
-            public void ShowProducts(List<Product> products,ConsoleColor colorText)
-            {
-                for (int i = 0; i < products.Count; i++)
-                {
-                    Console.ForegroundColor = colorText;
-                    Console.Write(i + 1 + " ");
-                    products[i].ShowInfo();
-                    Console.ForegroundColor = ConsoleColor.White;
-                }
-            }
+    }
 
-            public void ConsoleColorText(string text, ConsoleColor foreground)
+    class Persone
+    {
+        public void Products(List<Product> products)
+        {
+            products.Add(new Product("фото", 900));
+            products.Add(new Product("ноут", 200));
+            products.Add(new Product("тел", 50));
+        }
+
+        public void ShowProducts(List<Product> products, ConsoleColor colorText)
+        {
+            for (int i = 0; i < products.Count; i++)
             {
-                Console.ForegroundColor = foreground;
-                Console.WriteLine(text);
+                Console.ForegroundColor = colorText;
+                Console.Write(i + 1 + " ");
+                products[i].ShowInfo();
                 Console.ForegroundColor = ConsoleColor.White;
             }
         }
 
-        class Player : Persone
+        public void ConsoleColorText(string text, ConsoleColor foreground)
         {
-            private List<Product> soldGoods = new List<Product>();
-            public void BuyProduct(List<Product> products, int sell)
-            {
-                soldGoods.Add(products[sell - 1]);
-            }
+            Console.ForegroundColor = foreground;
+            Console.WriteLine(text);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
 
-            public void ShowPurchase()
-            {
-                if (soldGoods.Count == 0)
-                {
-                    ConsoleColorText("Покупок нет",ConsoleColor.Red);
-                }
-                else
-                {
-                    Console.WriteLine("Мои покупки");
-                    ShowProducts(soldGoods,ConsoleColor.Green);
-                }
-            }
+    class Player : Persone
+    {
+        private List<Product> soldGoods = new List<Product>();
+        private int _money = 1000;
+
+        public int GetMoney() => _money;
+
+        public int PayGoods(int price) => _money -= price;
+
+        public void BuyProduct(List<Product> products, int index)
+        {
+            soldGoods.Add(products[index - 1]);
         }
 
-        class Seller : Persone
+        public void ShowPurchase()
         {
-            public void SellProduct(List<Product> products, Seller seller, Player player)
+            if (soldGoods.Count == 0)
             {
-                Console.WriteLine("Выберите товар который хотите купить");
-                seller.ShowProducts(products,ConsoleColor.Cyan);
+                ConsoleColorText("Покупок нет", ConsoleColor.Red);
+            }
+            else
+            {
+                Console.WriteLine("Мои покупки");
+                ShowProducts(soldGoods, ConsoleColor.Green);
+            }
+        }
+    }
 
-                int.TryParse(Console.ReadLine(), out int sell);
+    class Seller : Persone
+    {
+        private int _cash = 0;
 
-                if (sell <= 0 || sell > products.Count)
+        public void SellProduct(List<Product> products, Seller seller, Player player)
+        {
+            Console.WriteLine("Выберите товар который хотите купить");
+            seller.ShowProducts(products, ConsoleColor.Cyan);
+
+            int.TryParse(Console.ReadLine(), out int index);
+
+            if (index <= 0 || index > products.Count)
+            {
+                ConsoleColorText("Товар не найден!!!", ConsoleColor.Red);
+            }
+            else
+            {
+                Console.WriteLine("Внесите деньги");
+                int.TryParse(Console.ReadLine(), out int price);
+
+                if (products[index - 1].Price == price)
                 {
-                    ConsoleColorText("Товар не найден!!!", ConsoleColor.Red);
+                    int balance = player.GetMoney();
+
+                    if (balance >= price)
+                    {
+                        balance = player.PayGoods(price);
+                        _cash += price;
+                        player.BuyProduct(products, index);
+                        Console.WriteLine($"Баланс покупателя = {balance} Баланс продавца = {_cash}");
+                        Console.WriteLine("Товар продан!!!");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Баланс покупателя = {balance}");
+                        Console.WriteLine("не достаточно денег");
+                    }
                 }
                 else
                 {
-                    player.BuyProduct(products, sell);
-                    Console.WriteLine("Товар продан!!!");
+                    Console.WriteLine("Товар не найден!");
                 }
             }
         }
@@ -131,17 +169,19 @@ namespace Shop
 
     class Product
     {
-        private string _nameProduct;
+        private string _commodity;
 
-        public Product(string nameProduct)
+        public Product(string commodity, int price)
         {
-            _nameProduct = nameProduct;
+            _commodity = commodity;
+            Price = price;
         }
+
+        public int Price { get; private set; }
 
         public void ShowInfo()
         {
-            Console.WriteLine(_nameProduct);
+            Console.WriteLine($"{_commodity} : {Price}");
         }
     }
 }
-
